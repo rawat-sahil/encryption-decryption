29718ba210fc67d1fa12e8c4369de50d725cfdc2
sahil <sahil17091@iiitd.ac.in>
2020-04-25 17:56:17 +0530

    genrand complete

---
+++ fput_encrypt_rsa.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ra_watt on 4/25/20.
+//
+
+#include "helper.h"
+
+
+int main(int argc ,char*argv[]){
+    if(argc==1){
+        std::cout<<"file name is required"<<std::endl;
+        return(-1);
+    }
+    fput(std::string(argv[1]));
+}
---
+++ helper.h
@@ -0,0 +1,37 @@
+//
+// Created by ra_watt on 4/25/20.
+//
+
+#ifndef SE_ASSIGNMENT_4_HELPER_H
+#define SE_ASSIGNMENT_4_HELPER_H
+#include<iostream>
+#include <stdlib.h>
+#include<string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include<fcntl.h>
+#include<errno.h>
+#include <fstream>
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include<openssl/hmac.h>
+#include <openssl/rand.h>
+#include <openssl/aes.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include<shadow.h>
+#include <pwd.h>
+
+void get_key_iv(unsigned char * key,unsigned char *iv,int uid);
+void gen_rand(std::string filename);
+void handleErrors();
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,unsigned char *iv, unsigned char *plaintext);
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,unsigned char *iv, unsigned char *ciphertext);
+
+
+#endif //SE_ASSIGNMENT_4_HELPER_H
